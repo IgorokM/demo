86c738f90476a6b5680d7ca2fc4f8a9e7125bc4e
igorokiscool <igorokiscool@gmail.com>
2021-03-21 22:07:50 +0300

    tree data

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,8 @@ import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatTreeModule} from '@angular/material/tree';
+import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
 
 import {AppComponent} from './app.component';
@@ -15,6 +17,8 @@ import {AppComponent} from './app.component';
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
+    MatTreeModule,
+    MatIconModule,
     MatButtonModule
   ],
   providers: [],
